1ba9fdbd603beeac78067525195fa4cb10586f2b
AminMardani79 <76900072+AminMardani79@users.noreply.github.com>
2022-02-15 13:00:40 +0330

    finish game

--- js/main.js
+++ js/main.js
@@ -1,19 +1,36 @@
 const game = () => {
+    // global vars in game method
     let pScore = 0;
     let cScore = 0;
+    let winnerScore = 0;
+    const introScreen = document.querySelector('.intro');
+    const match = document.querySelector('.match');
+    const replay = document.querySelector('.replay');
+    const finalScore = document.querySelector('.intro__winner-score-number');
+
 
     const startGame = () => {
-
+        // local vars
         const startbtn = document.querySelector('.intro__start');
-        const introScreen = document.querySelector('.intro');
-        const match = document.querySelector('.match');
+        const setScore = document.querySelector('.intro__set-winner-score');
         // click to start
         startbtn.onclick = () => {
-            introScreen.classList.add('fadeOut');
-            match.classList.add('fadeIn');
+                introScreen.classList.add('fadeOut');
+                match.classList.add('fadeIn');
+                replay.classList.add('fadeIn');
+            }
+            // replay game
+        replay.onclick = () => {
+            replayGame();
+            UpdateScore();
+        }
+        setScore.onclick = () => {
+            winnerScore = +finalScore.value;
+            swal(`WinnerScore is ${winnerScore}`);
         }
     }
     const playMatch = () => {
+        // local vars
         const playerOptions = document.querySelectorAll('.match__options button');
         const playerHand = document.querySelector('.match__player-hand');
         const computerHand = document.querySelector('.match__computer-hand');
@@ -41,12 +58,27 @@ const game = () => {
             });
         });
     }
+    const replayGame = () => {
+        introScreen.classList.remove('fadeOut');
+        match.classList.remove('fadeIn');
+        replay.classList.remove('fadeIn');
+        pScore = 0;
+        cScore = 0;
+        winnerScore = 1;
+        UpdateScore();
+    }
     const UpdateScore = () => {
         const playerScore = document.querySelector('.score__number_player');
         const computerScore = document.querySelector('.score__number_computer');
 
         playerScore.textContent = pScore;
         computerScore.textContent = cScore;
+        finalScore.value = winnerScore;
+    }
+    const IsFinishGame = () => {
+        if ((cScore === winnerScore || pScore === winnerScore) && (cScore > 0 || pScore > 0)) {
+            cScore > pScore ? swal("Game is over", "Computer wins the game").then(() => replayGame()) : swal("Game is over", "Player wins the game").then(() => replayGame());
+        }
     }
     const compareHands = (playerChoice, computerChoice) => {
         const winner = document.querySelector('.match__winner');
@@ -63,6 +95,7 @@ const game = () => {
                 winner.textContent = 'Computer wins';
                 cScore++;
                 UpdateScore();
+                IsFinishGame();
                 return;
             }
 
@@ -70,7 +103,7 @@ const game = () => {
                 winner.textContent = 'Player wins';
                 pScore++;
                 UpdateScore();
-                UpdateScore();
+                IsFinishGame();
                 return;
             }
         }
@@ -81,6 +114,7 @@ const game = () => {
                 winner.textContent = 'Player wins';
                 pScore++;
                 UpdateScore();
+                IsFinishGame();
                 return;
             }
 
@@ -88,6 +122,7 @@ const game = () => {
                 winner.textContent = 'Computer wins';
                 cScore++;
                 UpdateScore();
+                IsFinishGame();
                 return;
             }
         }
@@ -98,6 +133,7 @@ const game = () => {
                 winner.textContent = 'Player wins';
                 pScore++;
                 UpdateScore();
+                IsFinishGame();
                 return;
             }
 
@@ -105,11 +141,11 @@ const game = () => {
                 winner.textContent = 'Computer wins';
                 cScore++;
                 UpdateScore();
+                IsFinishGame();
                 return;
             }
         }
     }
-
 
 
     startGame();
